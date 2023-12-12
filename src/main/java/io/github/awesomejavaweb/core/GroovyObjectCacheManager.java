524a64befe80794ee117b9d3a617b8fd04818b28
codeboyzhou <imzhouchen@gmail.com>
2023-12-13 02:57:01 +0800

    chore(core): 支持自定义caffeine缓存对象配置

--- src/main/java/io/github/awesomejavaweb/core/GroovyObjectCacheManager.java
+++ src/main/java/io/github/awesomejavaweb/core/GroovyObjectCacheManager.java
@@ -12,18 +12,25 @@ public final class GroovyObjectCacheManager {
         throw new UnsupportedOperationException("Utility class should not be instantiated");
     }
 
-    private static final Cache<String, GroovyObject> CACHE = Caffeine.newBuilder()
-        .expireAfterAccess(Duration.ofDays(1))
-        .initialCapacity(1)
-        .maximumSize(10)
-        .build();
+    private static final Cache<String, GroovyObject> DEFAULT_CACHE;
+
+    static {
+        DEFAULT_CACHE = Caffeine.newBuilder().initialCapacity(1).maximumSize(10).expireAfterAccess(Duration.ofDays(1)).build();
+    }
+
+    private static Cache<String, GroovyObject> customCache;
+
+    public static void useCustomCache(Cache<String, GroovyObject> customCache) {
+        GroovyObjectCacheManager.customCache = customCache;
+    }
 
     public static GroovyObject getIfPresent(String key) {
-        return CACHE.getIfPresent(key);
+        return customCache == null ? DEFAULT_CACHE.getIfPresent(key) : customCache.getIfPresent(key);
     }
 
     public static void put(String key, GroovyObject groovyObject) {
-        CACHE.put(key, groovyObject);
+        Cache<String, GroovyObject> cache = customCache == null ? DEFAULT_CACHE : customCache;
+        cache.put(key, groovyObject);
     }
 
 }
