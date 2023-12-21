f09f5bbf639fc7c7c1e3e2a6af2b724ca1e2993d
codeboyzhou <imzhouchen@gmail.com>
2023-12-21 11:36:12 +0800

    refactor(main): 移除AbstractGroovyScriptSecurityChecker类

--- src/main/java/com/github/awesome/scripting/groovy/security/DefaultGroovyScriptSecurityChecker.java
+++ src/main/java/com/github/awesome/scripting/groovy/security/DefaultGroovyScriptSecurityChecker.java
@@ -1,15 +1,35 @@
 package com.github.awesome.scripting.groovy.security;
 
+import com.github.awesome.scripting.groovy.exception.GroovyScriptSecurityException;
+
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
-public class DefaultGroovyScriptSecurityChecker extends AbstractGroovyScriptSecurityChecker {
+public class DefaultGroovyScriptSecurityChecker implements GroovyScriptSecurityChecker {
 
     @Override
     public Collection<String> shouldInterceptKeywords() {
         DefaultShouldInterceptKeywords[] keywords = DefaultShouldInterceptKeywords.values();
         return Arrays.stream(keywords).map(DefaultShouldInterceptKeywords::getKeyword).collect(Collectors.toSet());
+    }
+
+    @Override
+    public void checkOrThrow(String script) {
+        Collection<String> keywords = shouldInterceptKeywords();
+        if (keywords == null || keywords.isEmpty()) {
+            return;
+        }
+        for (String keyword : keywords) {
+            if (script.contains(keyword)) {
+                final String errorMessage = String.format(
+                    "Groovy script contains potentially unsafe keyword: '%s', %s, please notice the following keywords are all considered unsafe: %s",
+                    keyword, Objects.requireNonNull(DefaultShouldInterceptKeywords.fromKeyword(keyword)).getExplanation(), keywords
+                );
+                throw new GroovyScriptSecurityException(errorMessage);
+            }
+        }
     }
 
 }
